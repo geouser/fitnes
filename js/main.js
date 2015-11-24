da6f2b3dd9337733f5264ad42b8ef1480e534d63
OlehRusyi <olehrusy@gmail,com>
2015-11-24 22:17:32 +0200

    вапролд

--- js/main.js
+++ js/main.js
@@ -71,7 +71,24 @@ jQuery(document).ready(function($) {
   });
 
   $('.slider').slick();
+  $('.offerSlider').slick();
 
+
+$(function() { // scroll to anchor
+    $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
+  });
 
 /*-----------------------------------------------------------------*/  
   $('.magnific').magnificPopup({
